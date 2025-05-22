0cf9312563d2b12e931291f49b3fa3e9970dd2fb
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-05-21 22:42:52 -0300

    Update my_heap.py

--- my_heap.py
+++ my_heap.py
@@ -17,6 +17,21 @@ class MaxHeap:
     def insert(self, value):
         self.heap.append(value)
         current = len(self.heap) - 1
+
         while current > 0 and self.heap[current] > self.heap[self._parent(current)]:
             self._swap(current, self._parent(current))
             current = self._parent(current)
+
+    def remove(self):
+        if len(self.heap) == 0:
+            return None
+    
+        if len(self.heap) == 1:
+            return self.heap.pop()
+            
+        max_value = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._sink_down(0)
+        
+        return max_value
+    
