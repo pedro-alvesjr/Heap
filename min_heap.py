dece459ab5ce21a92eef01607a056204d72533f8
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-05-24 21:22:12 -0300

    Update min_heap.py

--- min_heap.py
+++ min_heap.py
@@ -22,4 +22,15 @@ class MinHeap:
             self._swap(current, self._parent(current))
             current = self._parent(current)
 
-    
+    def remove(self):
+        if len(self.heap) == 0:
+            return None
+        
+        if len(self.heap) == 1:
+            return self.heap.pop()
+        
+        min_value = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._sink_down(0)
+
+        return min_value
